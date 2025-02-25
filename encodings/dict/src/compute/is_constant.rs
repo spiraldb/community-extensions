spiraldb/community-extensions

commit 5d10e733e32cbe295ebf5efb9fd3ccfffe13442b

Optional fallback in new is_constant compute function (#2485)

vortex-array/src/array/statistics.rs
@@ -3,7 +3,7 @@ use std::sync::RwLock;
 use vortex_error::{VortexExpect, VortexResult};
 use vortex_scalar::{Scalar, ScalarValue};
 
-use crate::compute::{is_constant, min_max, scalar_at, sum, MinMaxResult};
+use crate::compute::{is_constant_opts, min_max, scalar_at, sum, IsConstantOpts, MinMaxResult};
 use crate::stats::{Precision, Stat, Statistics, StatsSet};
 use crate::{Array, ArrayImpl};
 
@@ -86,7 +86,15 @@ impl<A: Array + ArrayImpl> Statistics for A {
                 if self.is_empty() {
                     None
                 } else {
-                    Some(is_constant(self)?.into())
+                    Some(
+                        is_constant_opts(
+                            self,
+                            &IsConstantOpts {
+                                fallback_to_canonicalize: true,
+                            },
+                        )?
+                        .into(),
+                    )
                 }
             }
             _ => {

vortex-array/src/arrays/list/compute/mod.rs
@@ -84,6 +84,7 @@ impl MinMaxFn<&ListArray> for ListEncoding {
 
 impl IsConstantFn<&ListArray> for ListEncoding {
     fn is_constant(&self, _array: &ListArray) -> VortexResult<Option<bool>> {
+        // TODO(adam): Do we want to fallback to arrow here?
         Ok(None)
     }
 }

vortex-array/src/compute/boolean.rs
@@ -7,6 +7,7 @@ use vortex_dtype::DType;
 use vortex_error::{vortex_bail, VortexExpect, VortexResult};
 
 use crate::arrow::{FromArrowArray, IntoArrowArray};
+use crate::compute::is_constant;
 use crate::encoding::Encoding;
 use crate::{Array, ArrayRef};
 
@@ -85,13 +86,15 @@ pub fn binary_boolean(
         vortex_bail!("Boolean operations are only supported on boolean arrays")
     }
 
+    let rhs_is_constant = is_constant(rhs)?;
+
     // If LHS is constant, then we make sure it's on the RHS.
-    if lhs.is_constant() && !rhs.is_constant() {
+    if is_constant(lhs)? && !rhs_is_constant {
         return binary_boolean(rhs, lhs, op);
     }
 
     // If the RHS is constant and the LHS is Arrow, we can't do any better than arrow_compare.
-    if lhs.is_arrow() && (rhs.is_arrow() || rhs.is_constant()) {
+    if lhs.is_arrow() && (rhs.is_arrow() || rhs_is_constant) {
         return arrow_boolean(lhs.to_array(), rhs.to_array(), op);
     }
 

vortex-array/src/compute/compare.rs
@@ -7,6 +7,7 @@ use vortex_dtype::{DType, NativePType, Nullability};
 use vortex_error::{vortex_bail, VortexExpect, VortexResult};
 use vortex_scalar::Scalar;
 
+use super::is_constant;
 use crate::arrays::ConstantArray;
 use crate::arrow::{from_arrow_array_with_len, Datum};
 use crate::encoding::Encoding;
@@ -124,8 +125,11 @@ pub fn compare(left: &dyn Array, right: &dyn Array, operator: Operator) -> Vorte
         return Ok(ConstantArray::new(Scalar::null(result_dtype), left.len()).into_array());
     }
 
+    let left_is_constant = is_constant(left)?;
+    let right_is_constant = is_constant(right)?;
+
     // Always try to put constants on the right-hand side so encodings can optimise themselves.
-    if left.is_constant() && !right.is_constant() {
+    if left_is_constant && !right_is_constant {
         return compare(right, left, operator.swap());
     }
 
@@ -151,7 +155,7 @@ pub fn compare(left: &dyn Array, right: &dyn Array, operator: Operator) -> Vorte
 
     // Only log missing compare implementation if there's possibly better one than arrow,
     // i.e. lhs isn't arrow or rhs isn't arrow or constant
-    if !(left.is_arrow() && (right.is_arrow() || right.is_constant())) {
+    if !(left.is_arrow() && (right.is_arrow() || right_is_constant)) {
         log::debug!(
             "No compare implementation found for LHS {}, RHS {}, and operator {} (or inverse)",
             right.encoding(),

vortex-array/src/compute/is_constant.rs
@@ -27,6 +27,16 @@ where
     }
 }
 
+#[derive(Default, Clone)]
+pub struct IsConstantOpts {
+    pub fallback_to_canonicalize: bool,
+}
+
+pub fn is_constant(array: &dyn Array) -> VortexResult<bool> {
+    let opts = IsConstantOpts::default();
+    is_constant_opts(array, &opts)
+}
+
 /// Computes whether an array has constant values.
 /// An array is constant IFF at least one of the following conditions apply:
 /// 1. It has one elements.
@@ -37,7 +47,7 @@ where
 ///
 /// If the array has some null values but is not all null, it'll never be constant.
 /// **Please note:** Might return false negatives if a specific encoding couldn't make a determination.
-pub fn is_constant(array: &dyn Array) -> VortexResult<bool> {
+pub fn is_constant_opts(array: &dyn Array, opts: &IsConstantOpts) -> VortexResult<bool> {
     match array.len() {
         // Our current semantics are that we can always get a value out of a constant array. We might want to change that in the future.
         0 => return Ok(false),
@@ -96,15 +106,19 @@ pub fn is_constant(array: &dyn Array) -> VortexResult<bool> {
             array.encoding()
         );
 
-        let array = array.to_canonical()?;
+        if opts.fallback_to_canonicalize {
+            let array = array.to_canonical()?;
 
-        if let Some(is_constant_fn) = array.as_ref().vtable().is_constant_fn() {
-            is_constant_fn.is_constant(array.as_ref())?
+            if let Some(is_constant_fn) = array.as_ref().vtable().is_constant_fn() {
+                is_constant_fn.is_constant(array.as_ref())?
+            } else {
+                vortex_bail!(
+                    "No is_constant function for canonical array: {}",
+                    array.as_ref().encoding(),
+                )
+            }
         } else {
-            vortex_bail!(
-                "No is_constant function for canonical array: {}",
-                array.as_ref().encoding(),
-            )
+            None
         }
     };
 

vortex-array/src/compute/slice.rs
@@ -1,5 +1,6 @@
 use vortex_error::{vortex_bail, vortex_err, VortexExpect, VortexResult};
 
+use crate::compute::is_constant;
 use crate::encoding::Encoding;
 use crate::stats::{Precision, Stat, StatsSet};
 use crate::{Array, ArrayRef, Canonical, IntoArray};
@@ -47,7 +48,8 @@ pub fn slice(array: &dyn Array, start: usize, stop: usize) -> VortexResult<Array
 
     // We know that constant array don't need stats propagation, so we can avoid the overhead of
     // computing derived stats and merging them in.
-    let derived_stats = (!array.is_constant()).then(|| derive_sliced_stats(array));
+    let array_is_constant = is_constant(array)?;
+    let derived_stats = (!array_is_constant).then(|| derive_sliced_stats(array));
 
     let sliced = array
         .vtable()

vortex-array/src/compute/take.rs
@@ -3,6 +3,7 @@ use vortex_scalar::Scalar;
 
 use crate::arrays::ConstantArray;
 use crate::builders::ArrayBuilder;
+use crate::compute::is_constant;
 use crate::encoding::Encoding;
 use crate::stats::{Precision, Stat, StatsSet};
 use crate::{Array, ArrayRef, IntoArray};
@@ -75,7 +76,8 @@ pub fn take(array: &dyn Array, indices: &dyn Array) -> VortexResult<ArrayRef> {
 
     // We know that constant array don't need stats propagation, so we can avoid the overhead of
     // computing derived stats and merging them in.
-    let derived_stats = (!array.is_constant()).then(|| derive_take_stats(array));
+    let array_is_constant = is_constant(array)?;
+    let derived_stats = (!array_is_constant).then(|| derive_take_stats(array));
 
     let taken = take_impl(array, indices)?;
 

encodings/alp/src/alp/compute/mod.rs
@@ -5,7 +5,7 @@ use std::fmt::Debug;
 use vortex_array::arrays::ConstantArray;
 use vortex_array::compute::{
     between, filter, scalar_at, slice, take, BetweenFn, BetweenOptions, CompareFn, FilterFn,
-    IsConstantFn, ScalarAtFn, SliceFn, StrictComparison, TakeFn,
+    ScalarAtFn, SliceFn, StrictComparison, TakeFn,
 };
 use vortex_array::variants::PrimitiveArrayTrait;
 use vortex_array::vtable::ComputeVTable;
@@ -27,10 +27,6 @@ impl ComputeVTable for ALPEncoding {
     }
 
     fn filter_fn(&self) -> Option<&dyn FilterFn<&dyn Array>> {
-        Some(self)
-    }
-
-    fn is_constant_fn(&self) -> Option<&dyn IsConstantFn<&dyn Array>> {
         Some(self)
     }
 
@@ -118,12 +114,6 @@ impl FilterFn<&ALPArray> for ALPEncoding {
             ALPArray::try_new(filter(array.encoded(), mask)?, array.exponents(), patches)?
                 .into_array(),
         )
-    }
-}
-
-impl IsConstantFn<&ALPArray> for ALPEncoding {
-    fn is_constant(&self, _array: &ALPArray) -> VortexResult<Option<bool>> {
-        Ok(None)
     }
 }
 

commit 324bd3d20c31240e1804bf220717337e8a98792f

one last is_constant followup (#2488)
There were still some ALP-related regression in #2485, I think this should fix it.

encodings/dict/src/compute/is_constant.rs
@@ -1,10 +1,10 @@
-use vortex_array::compute::{IsConstantFn, is_constant};
+use vortex_array::compute::{IsConstantFn, IsConstantOpts, is_constant_opts};
 use vortex_error::VortexResult;
 
 use crate::{DictArray, DictEncoding};
 
 impl IsConstantFn<&DictArray> for DictEncoding {
-    fn is_constant(&self, array: &DictArray) -> VortexResult<Option<bool>> {
-        is_constant(array.codes()).map(Some)
+    fn is_constant(&self, array: &DictArray, opts: &IsConstantOpts) -> VortexResult<Option<bool>> {
+        is_constant_opts(array.codes(), opts).map(Some)
     }
 }

vortex-array/src/array/statistics.rs
@@ -3,20 +3,30 @@ use std::sync::RwLock;
 use vortex_error::{VortexExpect, VortexResult};
 use vortex_scalar::{Scalar, ScalarValue};
 
-use crate::compute::{IsConstantOpts, MinMaxResult, is_constant_opts, min_max, scalar_at, sum};
+use crate::compute::{
+    IsConstantOpts, MinMaxResult, is_constant, is_constant_opts, min_max, scalar_at, sum,
+};
 use crate::stats::{Precision, Stat, Statistics, StatsSet};
 use crate::{Array, ArrayImpl};
 
 /// Extension functions for arrays that provide statistics.
 pub trait ArrayStatistics {
+    /// Make a best effort attempt to try and figure out if the array is constant, without canonicalizing it.
     fn is_constant(&self) -> bool;
 
+    /// If [`ArrayStatistics::is_constant`] is true, return the actual constant value as a [`Scalar`].
     fn as_constant(&self) -> Option<Scalar>;
 }
 
 impl<A: Array + 'static> ArrayStatistics for A {
     fn is_constant(&self) -> bool {
-        self.statistics().compute_is_constant().unwrap_or(false)
+        let opts = IsConstantOpts {
+            canonicalize: false,
+        };
+        is_constant_opts(self, &opts)
+            .inspect_err(|e| log::warn!("Failed to compute IsConstant: {e}"))
+            .ok()
+            .unwrap_or_default()
     }
 
     fn as_constant(&self) -> Option<Scalar> {
@@ -86,15 +96,7 @@ impl<A: Array + ArrayImpl> Statistics for A {
                 if self.is_empty() {
                     None
                 } else {
-                    Some(
-                        is_constant_opts(
-                            self,
-                            &IsConstantOpts {
-                                fallback_to_canonicalize: true,
-                            },
-                        )?
-                        .into(),
-                    )
+                    Some(is_constant(self)?.into())
                 }
             }
             _ => {

vortex-array/src/arrays/bool/compute/is_constant.rs
@@ -1,10 +1,10 @@
 use vortex_error::VortexResult;
 
 use crate::arrays::{BoolArray, BoolEncoding};
-use crate::compute::IsConstantFn;
+use crate::compute::{IsConstantFn, IsConstantOpts};
 
 impl IsConstantFn<&BoolArray> for BoolEncoding {
-    fn is_constant(&self, array: &BoolArray) -> VortexResult<Option<bool>> {
+    fn is_constant(&self, array: &BoolArray, _opts: &IsConstantOpts) -> VortexResult<Option<bool>> {
         let buffer = array.boolean_buffer();
 
         // Safety:
@@ -39,7 +39,9 @@ mod tests {
     fn test_is_constant(#[case] input: Vec<bool>, #[case] expected: Option<bool>) {
         let array = BoolArray::from_iter(input);
 
-        let output = BoolEncoding.is_constant(&array).unwrap();
+        let output = BoolEncoding
+            .is_constant(&array, &IsConstantOpts::default())
+            .unwrap();
         assert_eq!(output, expected);
     }
 }

vortex-array/src/arrays/chunked/compute/is_constant.rs
@@ -1,22 +1,26 @@
 use vortex_error::{VortexExpect, VortexResult};
 
 use crate::arrays::{ChunkedArray, ChunkedEncoding};
-use crate::compute::{IsConstantFn, is_constant, scalar_at};
+use crate::compute::{IsConstantFn, IsConstantOpts, is_constant_opts, scalar_at};
 
 impl IsConstantFn<&ChunkedArray> for ChunkedEncoding {
-    fn is_constant(&self, array: &ChunkedArray) -> VortexResult<Option<bool>> {
+    fn is_constant(
+        &self,
+        array: &ChunkedArray,
+        opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
         let mut chunks = array.chunks().iter();
 
         let first_chunk = chunks.next().vortex_expect("Must have at least one value");
 
-        if !is_constant(first_chunk)? {
+        if !is_constant_opts(first_chunk, opts)? {
             return Ok(Some(false));
         }
 
         let first_value = scalar_at(first_chunk, 0)?.into_nullable();
 
         for chunk in chunks {
-            if !is_constant(chunk)? {
+            if !is_constant_opts(chunk, opts)? {
                 return Ok(Some(false));
             }
 

vortex-array/src/arrays/extension/compute/mod.rs
@@ -8,8 +8,9 @@ use vortex_scalar::Scalar;
 use crate::arrays::ExtensionEncoding;
 use crate::arrays::extension::ExtensionArray;
 use crate::compute::{
-    CastFn, CompareFn, FilterFn, IsConstantFn, MinMaxFn, MinMaxResult, ScalarAtFn, SliceFn, SumFn,
-    TakeFn, ToArrowFn, filter, is_constant, min_max, scalar_at, slice, sum, take,
+    CastFn, CompareFn, FilterFn, IsConstantFn, IsConstantOpts, MinMaxFn, MinMaxResult, ScalarAtFn,
+    SliceFn, SumFn, TakeFn, ToArrowFn, filter, is_constant_opts, min_max, scalar_at, slice, sum,
+    take,
 };
 use crate::variants::ExtensionArrayTrait;
 use crate::vtable::ComputeVTable;
@@ -115,7 +116,11 @@ impl MinMaxFn<&ExtensionArray> for ExtensionEncoding {
 }
 
 impl IsConstantFn<&ExtensionArray> for ExtensionEncoding {
-    fn is_constant(&self, array: &ExtensionArray) -> VortexResult<Option<bool>> {
-        is_constant(array.storage()).map(Some)
+    fn is_constant(
+        &self,
+        array: &ExtensionArray,
+        opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
+        is_constant_opts(array.storage(), opts).map(Some)
     }
 }

vortex-array/src/arrays/list/compute/mod.rs
@@ -9,7 +9,8 @@ use vortex_scalar::Scalar;
 
 use crate::arrays::{ListArray, ListEncoding};
 use crate::compute::{
-    IsConstantFn, MaskFn, MinMaxFn, MinMaxResult, ScalarAtFn, SliceFn, ToArrowFn, scalar_at, slice,
+    IsConstantFn, IsConstantOpts, MaskFn, MinMaxFn, MinMaxResult, ScalarAtFn, SliceFn, ToArrowFn,
+    scalar_at, slice,
 };
 use crate::vtable::ComputeVTable;
 use crate::{Array, ArrayRef};
@@ -83,7 +84,11 @@ impl MinMaxFn<&ListArray> for ListEncoding {
 }
 
 impl IsConstantFn<&ListArray> for ListEncoding {
-    fn is_constant(&self, _array: &ListArray) -> VortexResult<Option<bool>> {
+    fn is_constant(
+        &self,
+        _array: &ListArray,
+        _opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
         // TODO(adam): Do we want to fallback to arrow here?
         Ok(None)
     }

vortex-array/src/arrays/primitive/compute/is_constant.rs
@@ -1,11 +1,15 @@
 use vortex_dtype::{NativePType, match_each_native_ptype};
 
 use crate::arrays::{PrimitiveArray, PrimitiveEncoding};
-use crate::compute::IsConstantFn;
+use crate::compute::{IsConstantFn, IsConstantOpts};
 use crate::variants::PrimitiveArrayTrait;
 
 impl IsConstantFn<&PrimitiveArray> for PrimitiveEncoding {
-    fn is_constant(&self, array: &PrimitiveArray) -> vortex_error::VortexResult<Option<bool>> {
+    fn is_constant(
+        &self,
+        array: &PrimitiveArray,
+        _opts: &IsConstantOpts,
+    ) -> vortex_error::VortexResult<Option<bool>> {
         let is_constant = match_each_native_ptype!(array.ptype(), |$P| {
             compute_is_constant(array.as_slice::<$P>())
         });

vortex-array/src/arrays/struct_/compute/mod.rs
@@ -9,8 +9,8 @@ use vortex_scalar::Scalar;
 use crate::arrays::StructEncoding;
 use crate::arrays::struct_::StructArray;
 use crate::compute::{
-    CastFn, FilterFn, IsConstantFn, MaskFn, MinMaxFn, MinMaxResult, ScalarAtFn, SliceFn, TakeFn,
-    ToArrowFn, filter, is_constant, scalar_at, slice, take, try_cast,
+    CastFn, FilterFn, IsConstantFn, IsConstantOpts, MaskFn, MinMaxFn, MinMaxResult, ScalarAtFn,
+    SliceFn, TakeFn, ToArrowFn, filter, is_constant_opts, scalar_at, slice, take, try_cast,
 };
 use crate::variants::StructArrayTrait;
 use crate::vtable::ComputeVTable;
@@ -176,14 +176,18 @@ impl MinMaxFn<&StructArray> for StructEncoding {
 }
 
 impl IsConstantFn<&StructArray> for StructEncoding {
-    fn is_constant(&self, array: &StructArray) -> VortexResult<Option<bool>> {
+    fn is_constant(
+        &self,
+        array: &StructArray,
+        opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
         let children = array.children();
         if children.is_empty() {
             return Ok(None);
         }
 
         for child in children.iter() {
-            if !is_constant(child)? {
+            if !is_constant_opts(child, opts)? {
                 return Ok(Some(false));
             }
         }

vortex-array/src/arrays/varbin/compute/is_constant.rs
@@ -3,10 +3,14 @@ use vortex_error::VortexResult;
 use crate::accessor::ArrayAccessor;
 use crate::arrays::varbin::stats::compute_is_constant;
 use crate::arrays::{VarBinArray, VarBinEncoding};
-use crate::compute::IsConstantFn;
+use crate::compute::{IsConstantFn, IsConstantOpts};
 
 impl IsConstantFn<&VarBinArray> for VarBinEncoding {
-    fn is_constant(&self, array: &VarBinArray) -> VortexResult<Option<bool>> {
+    fn is_constant(
+        &self,
+        array: &VarBinArray,
+        _opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
         array.with_iterator(compute_is_constant).map(Some)
     }
 }

vortex-array/src/arrays/varbinview/compute/is_constant.rs
@@ -1,10 +1,14 @@
 use vortex_error::{VortexExpect, VortexResult};
 
 use crate::arrays::{Ref, VarBinViewArray, VarBinViewEncoding};
-use crate::compute::IsConstantFn;
+use crate::compute::{IsConstantFn, IsConstantOpts};
 
 impl IsConstantFn<&VarBinViewArray> for VarBinViewEncoding {
-    fn is_constant(&self, array: &VarBinViewArray) -> VortexResult<Option<bool>> {
+    fn is_constant(
+        &self,
+        array: &VarBinViewArray,
+        _opts: &IsConstantOpts,
+    ) -> VortexResult<Option<bool>> {
         let mut views_iter = array.views().iter();
         let first_value = views_iter
             .next()

vortex-array/src/compute/boolean.rs
@@ -7,7 +7,6 @@ use vortex_dtype::DType;
 use vortex_error::{VortexExpect, VortexResult, vortex_bail};
 
 use crate::arrow::{FromArrowArray, IntoArrowArray};
-use crate::compute::is_constant;
 use crate::encoding::Encoding;
 use crate::{Array, ArrayRef};
 
@@ -97,10 +96,10 @@ pub fn binary_boolean(
         )
     }
 
-    let rhs_is_constant = is_constant(rhs)?;
+    let rhs_is_constant = rhs.is_constant();
 
     // If LHS is constant, then we make sure it's on the RHS.
-    if is_constant(lhs)? && !rhs_is_constant {
+    if lhs.is_constant() && !rhs_is_constant {
         return binary_boolean(rhs, lhs, op);
     }
 

vortex-array/src/compute/compare.rs
@@ -7,7 +7,6 @@ use vortex_dtype::{DType, NativePType, Nullability};
 use vortex_error::{VortexExpect, VortexResult, vortex_bail};
 use vortex_scalar::Scalar;
 
-use super::is_constant;
 use crate::arrays::ConstantArray;
 use crate::arrow::{Datum, from_arrow_array_with_len};
 use crate::encoding::Encoding;
@@ -126,11 +125,10 @@ pub fn compare(left: &dyn Array, right: &dyn Array, operator: Operator) -> Vorte
         return Ok(ConstantArray::new(Scalar::null(result_dtype), left.len()).into_array());
     }
 
-    let left_is_constant = is_constant(left)?;
-    let right_is_constant = is_constant(right)?;
+    let right_is_constant = right.is_constant();
 
     // Always try to put constants on the right-hand side so encodings can optimise themselves.
-    if left_is_constant && !right_is_constant {
+    if left.is_constant() && !right_is_constant {
         return compare(right, left, operator.swap());
     }
 

vortex-array/src/compute/is_constant.rs
@@ -11,33 +11,36 @@ pub trait IsConstantFn<A> {
     /// * array.len() > 1
     ///
     /// Returns `Ok(None)` to signal we couldn't make an exact determination.
-    fn is_constant(&self, array: A) -> VortexResult<Option<bool>>;
+    fn is_constant(&self, array: A, opts: &IsConstantOpts) -> VortexResult<Option<bool>>;
 }
 
 impl<E: Encoding> IsConstantFn<&dyn Array> for E
 where
     E: for<'a> IsConstantFn<&'a E::Array>,
 {
-    fn is_constant(&self, array: &dyn Array) -> VortexResult<Option<bool>> {
+    fn is_constant(&self, array: &dyn Array, opts: &IsConstantOpts) -> VortexResult<Option<bool>> {
         let array_ref = array
             .as_any()
             .downcast_ref::<E::Array>()
             .vortex_expect("Failed to downcast array");
-        IsConstantFn::is_constant(self, array_ref)
+        IsConstantFn::is_constant(self, array_ref, opts)
     }
 }
 
-#[derive(Default, Clone)]
+/// Configuration for [`is_constant_opts`] operations.
+#[derive(Clone)]
 pub struct IsConstantOpts {
-    pub fallback_to_canonicalize: bool,
+    /// Should the operation make an effort to canonicalize the target array if its encoding doesn't implement [`IsConstantFn`].
+    pub canonicalize: bool,
 }
 
-pub fn is_constant(array: &dyn Array) -> VortexResult<bool> {
-    let opts = IsConstantOpts::default();
-    is_constant_opts(array, &opts)
+impl Default for IsConstantOpts {
+    fn default() -> Self {
+        Self { canonicalize: true }
+    }
 }
 
-/// Computes whether an array has constant values.
+/// Computes whether an array has constant values. If the array's encoding doesn't implement the relevant VTable, it'll try and canonicalize in order to make a determination.
 /// An array is constant IFF at least one of the following conditions apply:
 /// 1. It has one elements.
 /// 1. Its encoded as a [`ConstantArray`] or [`NullArray`]
@@ -47,6 +50,14 @@ pub fn is_constant(array: &dyn Array) -> VortexResult<bool> {
 ///
 /// If the array has some null values but is not all null, it'll never be constant.
 /// **Please note:** Might return false negatives if a specific encoding couldn't make a determination.
+pub fn is_constant(array: &dyn Array) -> VortexResult<bool> {
+    let opts = IsConstantOpts::default();
+    is_constant_opts(array, &opts)
+}
+
+/// Computes whether an array has constant values. Configurable by [`IsConstantOpts`].
+///
+/// Please see [`is_constant`] for a more detailed explanation of its behavior.
 pub fn is_constant_opts(array: &dyn Array, opts: &IsConstantOpts) -> VortexResult<bool> {
     match array.len() {
         // Our current semantics are that we can always get a value out of a constant array. We might want to change that in the future.
@@ -99,18 +110,18 @@ pub fn is_constant_opts(array: &dyn Array, opts: &IsConstantOpts) -> VortexResul
         "All values must be valid as an invariant of the VTable."
     );
     let is_constant = if let Some(vtable_fn) = array.vtable().is_constant_fn() {
-        vtable_fn.is_constant(array)?
+        vtable_fn.is_constant(array, opts)?
     } else {
         log::debug!(
             "No is_constant implementation found for {}",
             array.encoding()
         );
 
-        if opts.fallback_to_canonicalize {
+        if opts.canonicalize {
             let array = array.to_canonical()?;
 
             if let Some(is_constant_fn) = array.as_ref().vtable().is_constant_fn() {
-                is_constant_fn.is_constant(array.as_ref())?
+                is_constant_fn.is_constant(array.as_ref(), opts)?
             } else {
                 vortex_bail!(
                     "No is_constant function for canonical array: {}",

vortex-array/src/compute/slice.rs
@@ -1,6 +1,5 @@
 use vortex_error::{VortexExpect, VortexResult, vortex_bail, vortex_err};
 
-use crate::compute::is_constant;
 use crate::encoding::Encoding;
 use crate::stats::{Precision, Stat, StatsSet};
 use crate::{Array, ArrayRef, Canonical, IntoArray};
@@ -48,8 +47,7 @@ pub fn slice(array: &dyn Array, start: usize, stop: usize) -> VortexResult<Array
 
     // We know that constant array don't need stats propagation, so we can avoid the overhead of
     // computing derived stats and merging them in.
-    let array_is_constant = is_constant(array)?;
-    let derived_stats = (!array_is_constant).then(|| derive_sliced_stats(array));
+    let derived_stats = (!array.is_constant()).then(|| derive_sliced_stats(array));
 
     let sliced = array
         .vtable()

vortex-array/src/compute/take.rs
@@ -3,7 +3,6 @@ use vortex_scalar::Scalar;
 
 use crate::arrays::ConstantArray;
 use crate::builders::ArrayBuilder;
-use crate::compute::is_constant;
 use crate::encoding::Encoding;
 use crate::stats::{Precision, Stat, StatsSet};
 use crate::{Array, ArrayRef, IntoArray};
@@ -76,8 +75,7 @@ pub fn take(array: &dyn Array, indices: &dyn Array) -> VortexResult<ArrayRef> {
 
     // We know that constant array don't need stats propagation, so we can avoid the overhead of
     // computing derived stats and merging them in.
-    let array_is_constant = is_constant(array)?;
-    let derived_stats = (!array_is_constant).then(|| derive_take_stats(array));
+    let derived_stats = (!array.is_constant()).then(|| derive_take_stats(array));
 
     let taken = take_impl(array, indices)?;
 